20a4d4d23eb942ad967543d8dce699f7dc305211
Ogheneminebabs <babatundelawal145@gmail.com>
2022-03-10 07:22:04 -0800

    Intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main(void)
+
 /**
  *main - Main function of my program
  *
  *
  *Return: return 0 everytime
  */
+
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
