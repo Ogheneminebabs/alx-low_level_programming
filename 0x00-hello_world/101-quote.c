1619506b57a2cf2d22109bfba3abcba3ba4a3240
Ogheneminebabs <babatundelawal145@gmail.com>
2022-03-10 07:44:13 -0800

    UNIX_is_a_simple_operating_system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+/**
+ *main - Main function for my project
+ *
+ *
+ *Return: return 0 everytime
+ */
+
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
