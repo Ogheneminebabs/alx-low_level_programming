3959f4150f7452574c4745aead43803e413d9c5e
Ogheneminebabs <babatundelawal145@gmail.com>
2022-03-22 07:57:28 -0700

    Print_a_string_in_reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,13 @@ void print_rev(char *s)
 {
 int fcounter = 0;
 int i, n;
+for (i = 0; s[i] != '\0'; i++);
 
-for (i = 0; s[i] != '\0'; i++;)
 {
 fcounter++;
 }
-for (n = (fcounter - 1); n >= 0; n--;)
+for (n = (fcounter - 1); n >= 0; n--);
+
 {
 putchar(s[n]);
 }
