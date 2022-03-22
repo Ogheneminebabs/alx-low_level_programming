25de66be9e249950f6a682171deafb57dbf2ebf8
Ogheneminebabs <babatundelawal145@gmail.com>
2022-03-22 05:30:13 -0700

    98_battery_street

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,15 +6,19 @@
  * Return: Always 0.
  */
 
-int main(void)
+void reset_to_98(int *n);
 {
 
 int n;
+int *p;
 
 n = 402;
+p = &n
+printf("n = %d\n", n);
+
+*p = 98;
 printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
+
 return (0);
 
 }
