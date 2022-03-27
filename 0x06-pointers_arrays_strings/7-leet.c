59419239d86e4ad85d3ae12fa83b4909dcc8de07
Lawal Oghenemine Babatunde <99467281+Ogheneminebabs@users.noreply.github.com>
2022-03-27 01:13:57 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 
 /**
- * rot13 - Entry point
+ * leet - Entry point
  * ONE if, TWO loops only...
  * @n: input
- * Return: decrypted string
+ * Return: Always 0 (Success)
  */
- 
-char *rot13(char *n)
-{
-int x, rot_c = 13, i = 0;
-char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P', 'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
 
-while (n[i] != '\0')
+char *leet(char *n)
 {
-for (x = 0; x <= 51; x++)
+int i, x;
+int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+int replacer[] = {'4', '3', '0', '7', '1'};
+for (i = 0; n[i] != '\0'; i++)
 {
-if (n[i] == toswap[x])
+for (x = 0; x <= 9; x++)
 {
-n[i] = n[i] + rot_c;
-x = 51;
+if (n[i] == find[x])
+{
+n[i] = replacer[x / 2];
+x = 9;
 }
-rot_c = rot_c * -1;
 }
-i++;
 }
+
 return (n);
 }
 
